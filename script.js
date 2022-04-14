6e6be48ad616218c556f046e1dd8df27ba6595c9
Abdennour Hachemi <nouri89@hotmail.com>
2022-04-14 12:59:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,116 +1,137 @@
+// /* 
+// “In a language of your choice, using all best practice principles you’re aware of,
+//  could you please provide code that iterates in multiples of A until X, then in multiples of A + 1 until 2X,
+//   then multiples of A + 2 until 3X. Please state any assumptions you’ve made.
+
+// Please upload to a public Github repository and share the link.”
+
+// /********************************************************************************************************** */
 /*
+-The user will enter positive numbers.
 
-                     **************** TASK *************************
-“In a language of your choice, using all best practice principles you’re aware of,
- could you please provide code that iterates in multiples of A until X, then in multiples of A + 1 until 2X,
-  then multiples of A + 2 until 3X. Please state any assumptions you’ve made.
-Please upload to a public Github repository and share the link.”
+-The result of multiplications will include the X,2X and 3X.
 
-/********************************************************************************************************** */
-/*******************************************************************************************************************
-                 The approach taken to solve this challenge is to :
-1-create an empty array to save the result of every multiplication.
-2-Check if A or X are greater then zero if not an error message is thrown.
-3-use a for loop to iterate in multiples starting the iteration from 1 and incrementing it by 1 to provide multiples.
-4-test the three conditions :
- -multiples of A until X.
- -multiples of A+1 until 2X.
- -multiples of A+2 until 3X.
- and store them accordingly 
-**********************************************************************************************************************/
+A front end was created using HTML and CSS files to take the entries from the user.
+
+The approach taken to solve this challenge was to :
+
+1-Create an empty array to save the result of every multiplication.
+
+2-store the initials and the conditions in two separate arrays.
+
+3-Create a call back function to generate the multiplication and store it in the resultArray according to the conditions to avoid repetition.
+
+4-call the function to generate the multiples using a ForEach loop.
+
+5-store the multiplication in the resultArray.
+
+6-Display the result in the screen.
+
+-A refresh button is created to clear the content.
+
+-A sorting buttons is added to sort the result ascendent descendent.
+
+-A remove duplicate button is also added.
+
+Thank you very much.
+
+
+*/
+
 
 
 let showResult = document.getElementById("generate");
-let resultDiv = document.getElementById("result");
+let resultDiv=document.getElementById("result");
 
-let spaceA = document.querySelector("#digitA");
-let spaceB = document.getElementById("digitX");
-let resultArray = []; // An array where to store the result of the multiplications
+let spaceA= document.querySelector("#digitA");
+let spaceB =document.getElementById("digitX");
+let resultArray =[] ;
 
 
 showResult.addEventListener("click",
-	function multiples() {
-		   
-		resultDiv.innerHTML = resultArray;
-		let A = parseInt(document.getElementById("digitA").value)
-		let X = parseInt(document.getElementById("digitX").value)
-		
-		if (!A || !X || typeof A != "number" || typeof X != "number" ) {
-			resultDiv.innerHTML = "Please Enter a valid digit"
-			return;
-		}
-		if (A <= 0 || X <= 0) { // An entry validation to make sure the multiplications does not generate zero
-			resultDiv.innerHTML = "Please Enter a positive number greater then Zero"
-			return;
-		}
-	
-	        // store the initials and the conditions in an array
-  		 let from =[A,A+1,A+2];
-  		 let to=[X,2*X,3*X];
 
+function multiples (){
+  //  let resultArray =[]     // An array where to store the result of the multiplications
+    resultDiv.innerHTML=resultArray;
+    let A = parseInt(document.getElementById("digitA").value)
+    let X =parseInt(document.getElementById("digitX").value) 
+    if(A<=0||X<=0){   // An entry validation to make sure the multiplications does not generate zero
+        resultDiv.innerHTML="Please Enter a positive number greater then Zero"
+        return;
+    }
 
- 		  // Create a function to generate the multiplication  and store it in the resultArray according to the conditions
-  		 function runTheLoop( start, condition){
-   			 for(let i=1;i*start<=condition;i++){       
-               			 resultArray.push(i*start)// store the multiplication in the resultArray
-           		 }
-   		}
    
-   		//call the function to generate the multiples using a ForEach loop
-  		 from.forEach((element,index)=>runTheLoop(element,to[index]))
 
-		
+     if(typeof A!="number" ||typeof X!="number"){ 
+        resultDiv.innerHTML="Please Enter a valid digit"
+        return;
+     }
+   
+   // store the initials and the conditions in an array
+   let from =[A,A+1,A+2];
+   let to=[X,2*X,3*X];
 
-		
-		if (resultArray.length == 0) {
+  
+   // Create a function to generate the multiplication  and store it in the resultArray according to the conditions
+   function runTheLoop( start, condition){
+    for(let i=1;i*start<=condition;i++){       
+                resultArray.push(i*start)// store the multiplication in the resultArray
+            }
+   }
+   
+   //call the function to generate the multiples using a ForEach loop
+   from.forEach((element,index)=>runTheLoop(element,to[index]))
 
-			resultDiv.innerHTML = `There are no Multiples found for digit A=${A} and digit X=${X}`;
-		} else {
-			resultDiv.innerText = resultArray;
-		}
-	});
+   
+  
 
-let clearResult = document.getElementById("refresh");
-refresh.addEventListener("click", function () {
-	resultArray = [];
-	resultDiv.innerHTML = "";
-	spaceA.value = "";
-	spaceB.value = ""
-	document.getElementById("digitA").value = "";
-	document.getElementById("digitX").value = "";
+
+    
+
+    if(resultArray.length==0){
+        
+        resultDiv.innerHTML=`There are no Multiples found for digit A=${A} and digit X=${X}`;
+    }
+    else{
+        resultDiv.innerText=resultArray;
+    }
+    
+        
+});
+
+let clearResult=document.getElementById("refresh");
+refresh.addEventListener("click",function(){
+   
+    resultArray=[];
+    resultDiv.innerHTML="";
+    spaceA.value="";
+    spaceB.value=""
+   document.getElementById("digitA").value="";
+   document.getElementById("digitX").value="";
+
 })
+let sortAsc=document.getElementById("sortAscend");
+sortAsc.addEventListener("click",function(){
+    
 
-let sortAsc = document.getElementById("sortAscend");
-sortAsc.addEventListener("click", function () {
-	if (resultArray.length==0 ) {
-			resultDiv.innerHTML = "No Numbers to sort"
-			return;
-		}
-	resultArray = resultArray.sort((a, b) => a - b)
-	resultDiv.innerText = resultArray;
-
+    resultArray= resultArray.sort((a,b)=>a-b)
+    resultDiv.innerText=resultArray;
+   
 })
-let sortDesc = document.getElementById("sortDescend");
-sortDesc.addEventListener("click", function () {
-	if (resultArray.length==0 ) {
-			resultDiv.innerHTML = "No Numbers to sort"
-			return;
-		}
-	resultArray = resultArray.sort((a, b) => b - a)
-	resultDiv.innerText = resultArray;
+let sortDesc=document.getElementById("sortDescend");
+sortDesc.addEventListener("click",function(){
+    
+
+    resultArray= resultArray.sort((a,b)=>b-a)
+    resultDiv.innerText=resultArray;
+   
 })
 
 // a function which filters the duplicates
-let remove = document.getElementById("removeDuplica");
-remove.addEventListener("click", function () {
-	if (resultArray.length==0 ) {
-			resultDiv.innerHTML = "No Duplicates to Remove"
-			return;
-		}
-
-	resultArray = resultArray.filter((number, index) => {
-		return resultArray.indexOf(number) === index
-	})
-	resultDiv.innerText = resultArray;
-
+let remove=document.getElementById("removeDuplica");
+remove.addEventListener("click",function(){
+    
+    resultArray= resultArray.filter((number,index)=> {return resultArray.indexOf(number)===index})
+   resultDiv.innerText=resultArray;
+   
 })
